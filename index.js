5d4e207b9d73d304c6c4249186088f672aec0052
Tom Nielsen <tanielsen@gmail.com>
2023-06-18 22:17:15 +0100

    callback view

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Workflow = require("@saltcorn/data/models/workflow");
 const Form = require("@saltcorn/data/models/form");
+const View = require("@saltcorn/data/models/view");
 const User = require("@saltcorn/data/models/user");
 const { getState } = require("@saltcorn/data/db/state");
 const db = require("@saltcorn/data/db");
@@ -43,6 +44,7 @@ const actions = ({ publishableKey, secretKey }) => ({
   mauapay_paymeent_request: {
     configFields: async ({ table }) => {
       const fields = table ? await table.getFields() : [];
+      const cbviews = await View.find({ viewtemplate: "MauaPay Callback" });
       return [
         {
           name: "order_id_field",
@@ -53,6 +55,7 @@ const actions = ({ publishableKey, secretKey }) => ({
             options: fields.map((f) => f.name),
           },
         },
+
         {
           name: "amount_field",
           label: "Amount field",
@@ -64,39 +67,57 @@ const actions = ({ publishableKey, secretKey }) => ({
               .map((f) => f.name),
           },
         },
+        {
+          name: "callback_view",
+          label: "Callback view",
+          type: "String",
+          required: true,
+          attributes: {
+            options: cbviews.map((f) => f.name),
+          },
+        },
       ];
     },
     run: async ({
       req,
       row,
-      configuration: { order_id_field, amount_field },
+      configuration: { order_id_field, amount_field, callback_view },
     }) => {
       const form = new FormData();
       form.append("orderID", row[order_id_field]);
       form.append("amount", row[amount_field]);
-      const fres = await fetch("https://api.mauapay.com/api/v1/transactions", {
-        method: "post",
-        body: form,
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          "x-business-publishable-key": publishableKey,
-        },
-      });
+      const fetchres = await fetch(
+        "https://api.mauapay.com/api/v1/transactions",
+        {
+          method: "post",
+          body: form,
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "x-business-publishable-key": publishableKey,
+          },
+        }
+      );
     },
   },
 });
 
-/*const viewtemplates = (config) => {
- 
-
+const viewtemplates = ({ publishableKey, secretKey }) => {
   return [
-   
+    {
+      name: "MauaPay Callback",
+      display_state_form: false,
+      configuration_workflow: () => new Workflow({ steps: [] }),
+      get_state_fields: () => [],
+      run: async (table_id, viewname, view_cfg, state, { req }) => {
+        return "";
+      },
+    },
   ];
-};*/
+};
 
 module.exports = {
   sc_plugin_api_version: 1,
   configuration_workflow,
   actions,
-  // viewtemplates,
+  viewtemplates,
 };
